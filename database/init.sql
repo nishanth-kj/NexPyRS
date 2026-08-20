229ae1ac1cfb5835fa63a55936f8ef25086c3a8b
nishanth-kj <nishanthkj12@gmail.com>
2026-08-20 22:35:56 +0530

    add database

---
+++ database/init.sql
@@ -0,0 +1,11 @@
+-- Environment specific databases
+CREATE DATABASE nexpyrs_dev;
+CREATE DATABASE nexpyrs_stage;
+CREATE DATABASE nexpyrs_prod;
+
+-- Microservice isolated databases
+CREATE DATABASE nexpyrs_fastapi;
+CREATE DATABASE nexpyrs_django;
+CREATE DATABASE nexpyrs_spring;
+CREATE DATABASE nexpyrs_actix;
+CREATE DATABASE nexpyrs_dragon;
